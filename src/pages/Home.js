df4958d41137507f9292da73fd175fb37e6686ad
Mark Radomski <mrad@email.com>
2019-01-22 09:24:12 +0700

    remove old imports

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import Main from 'components/Main';
 import HeroCarousel from 'components/HeroCarousel';
-import styled from 'styled-components';
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
 
 const slideshowPath = `${process.env.PUBLIC_URL}/assets/img/slideshow/`;
 
